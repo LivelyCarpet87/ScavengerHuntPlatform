3749d3411426f1b686b5e90d41d2c871e310be05
Tyllis Xu <LivelyCarpet87@gmail.com>
2025-11-13 09:41:29 -0600

    Color-blind friendliness for unsolved challenges

--- scavenger-hunt-platform/app/(event)/feedback/scoringReport.tsx
+++ scavenger-hunt-platform/app/(event)/feedback/scoringReport.tsx
@@ -27,8 +27,8 @@ export default function ScoringReport(props:any){
     }
 
     let solveState = (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='fill-slate-300 w-5'>
-            <path d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className='fill-slate-300 w-5'>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
         </svg>
     )
     if (props.approval== 1) {
