109d105742cea8f226bbd3baac062181907343af
Tyllis Xu <LivelyCarpet87@gmail.com>
2025-12-06 22:06:31 -0600

    Add apple icon

--- scavenger-hunt-platform/app/layout.tsx
+++ scavenger-hunt-platform/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
